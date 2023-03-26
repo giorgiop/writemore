c3615247ee557e136e9b24cf3f27815b7135eee3
giorgiop <giorgiop@users.noreply.github.com>
2023-03-26 18:53:24 +0200

    moved code and first tests

---
+++ tests/test_e2e.py
@@ -0,0 +1,14 @@
+import os
+from langchain.llms.fake import FakeListLLM
+
+from writemore.writemore import Writer
+
+os.environ["OPENAI_API_KEY"] = ""
+
+responses=[
+    "Something something"
+]
+llm = FakeListLLM(responses=responses)
+
+prompt = "test"
+writer = Writer(prompt, output_path="", llm=llm, verbose=False)
