734ee5a2892618c6513eaf9f5200b2face4b8a87
Ajinkya Indulkar <26824103+ajndkr@users.noreply.github.com>
2023-04-10 20:47:32 +0200

    :recycle: refactor scheduler and executor
    use `LLMChain` as parent class and `from_llm` classmethod to initialise class

--- writemore/templates.py
+++ writemore/templates.py
@@ -1,18 +1,19 @@
 CREATE_NEW_TASKS_TEMPLATE = """
-    You must create at most {n_tasks} tasks with the following objective: {objective},
-    The last completed task has was {task_description} with this result: {result}
-    These are incomplete tasks: {task_list} and new tasks must not overlap.
-    Return the tasks in a comma separated list, like: task1, task2, task3, ...
-    """
+You must create at most {n_tasks} tasks with the following objective: {objective},
+The last completed task has was {task_description} with this result: {result}
+These are incomplete tasks: {task_list} and new tasks must not overlap.
+Return the tasks in a comma separated list, like: task1, task2, task3, ...
+"""
 
-RESCHEDULE_TEMPLATE = """
-    You are tasked with prioritizing the following tasks: {task_list}.
-    Consider the ultimate objective of these tasks: {objective}.
-    Return the tasks in a comma separated list, like: task1, task2, task3, ...
-    Clean formatting of the text. Do not remove any task.
-    """
+RESCHEDULE_TASKS_TEMPLATE = """
+You are tasked with prioritizing the following tasks: {task_list}.
+Consider the ultimate objective of these tasks: {objective}.
+Return the tasks in a comma separated list, like: task1, task2, task3, ...
+Clean formatting of the text. Do not remove any task.
+"""
 
 EXECUTION_TEMPLATE = """
-    You must perform one task based on the following objective: {objective}\n.
-    Take into account these previously completed tasks: NOTHING\n.
-    Your task: {task}\nResponse:"""
+You must perform one task based on the following objective: {objective}.
+Take into account these previously completed tasks: NOTHING.
+Your task: {task}
+Response:"""
--- writemore/writemore.py
+++ writemore/writemore.py
@@ -1,114 +1,14 @@
-from collections import deque
-
-from langchain import LLMChain, PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from langchain.output_parsers import CommaSeparatedListOutputParser
 
-from writemore.templates import (
-    CREATE_NEW_TASKS_TEMPLATE,
-    EXECUTION_TEMPLATE,
-    RESCHEDULE_TEMPLATE,
-)
-
-VERBOSE = False
+from writemore.chains import Executor, Scheduler
 
 
-class Executor:
-    def __init__(self, objective, memory, llm):
-        self.objective = objective
-        # self.memory = memory
-        self.llm = llm
+def writemore(objective, first_task, verbose):
+    """Run writemore."""
+    llm = ChatOpenAI(temperature=0, max_tokens=500, verbose=verbose)
 
-    def run(self, task):
-        """Call LLM to execute the task"""
-
-        # context = context_agent(query=objective, n=5)
-
-        prompt = PromptTemplate(
-            template=EXECUTION_TEMPLATE,
-            input_variables=["objective", "task"],
-        )
-        self.chain = LLMChain(prompt=prompt, llm=self.llm, verbose=VERBOSE)
-        response = self.chain.run(objective=self.objective, task=task)
-        return response
-
-
-class Scheduler:
-    def __init__(self, objective, first_task, memory, llm):
-        self.objective = objective
-        self.memory = memory
-        self.llm = llm
-        self.task_list = deque([first_task])
-        self.output_parser = CommaSeparatedListOutputParser()
-
-    def next_task(self):
-        return self.task_list.popleft()
-
-    def add(self, task):
-        self.task_list.append(task)
-
-    def create_new_tasks(self):
-        """Check task_list. Call LLM to decide if new tasks must be added"""
-
-        prompt = PromptTemplate(
-            template=CREATE_NEW_TASKS_TEMPLATE,
-            input_variables=[
-                "objective",
-                "result",
-                "task_description",
-                "task_list",
-                "n_tasks",
-            ],
-        )
-        self.chain = LLMChain(prompt=prompt, llm=self.llm, verbose=VERBOSE)
-        response = self.chain.run(
-            objective=self.objective,
-            result="None",
-            task_description="None",
-            task_list={", ".join([str(t) for t in self.task_list])},
-            n_tasks=5,
-        )
-        new_tasks = self.output_parser.parse(response)
-        self.task_list.extend(new_tasks)
-        return self.task_list
-
-    def reschedule(self):
-        """Check task_list. Call LLM to decide if task priority is ok"""
-
-        prompt = PromptTemplate(
-            template=RESCHEDULE_TEMPLATE,
-            input_variables=["objective", "task_list"],
-        )
-        self.chain = LLMChain(prompt=prompt, llm=self.llm, verbose=VERBOSE)
-        response = self.chain.run(
-            objective=self.objective,
-            task_list={", ".join([str(t) for t in self.task_list])},
-        )
-
-        self.task_list = deque()
-        new_tasks = self.output_parser.parse(response)
-        self.task_list.extend(new_tasks)
-        return self.task_list
-
-
-# class Memory():
-
-#     def __init__(self, objective):
-#         self.objective = objective
-
-#     def fetch_context(self, n):
-#         context = context_agent(query=self.objective, n=n)
-
-
-def writemore(objective, task, verbose):
-    if verbose:
-        VERBOSE = True
-
-    llm = ChatOpenAI(temperature=0, max_tokens=500, verbose=VERBOSE)
-    # memory = Memory()
-    memory = []
-    scheduler = Scheduler(objective, task, memory, llm)
-    executor = Executor(objective, memory, llm)
+    scheduler = Scheduler.from_llm(objective, first_task, llm, verbose=verbose)
+    executor = Executor.from_llm(llm, verbose=verbose)
 
     iter, max_iter = 0, 5
     while scheduler.task_list or iter >= max_iter:
@@ -116,11 +16,10 @@ def writemore(objective, task, verbose):
         for t in scheduler.task_list:
             print(str(t))
 
-        task = scheduler.next_task()
+        first_task = scheduler.next_task()
         print("\n****Next task****\n")
-        print(task)
-        result = executor.run(task)
-        # memory.append(result)
+        print(first_task)
+        result = executor.run(first_task)
         print("\n****Task result****\n")
         print(result)
 
