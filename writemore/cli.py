d0bf79c6a35bbadc092b5112c7debda12b710022
Brian Thorne <brian@thorne.link>
2023-04-13 16:26:38 +1200

    Allow switching the LLM model

--- writemore/cli.py
+++ writemore/cli.py
@@ -10,7 +10,8 @@ class WriteMoreCLISettings(BaseSettings):
     OPENAI_API_KEY: str
 
     OBJECTIVE: Optional[str]
-    TASK: Optional[str]
+    TASK: Optional[str] = "Come up with a plan"
+    OPENAI_MODEL_NAME: Optional[str] = "gpt-3.5-turbo"
 
 
 settings = WriteMoreCLISettings()
@@ -25,10 +26,11 @@ settings = WriteMoreCLISettings()
     default=lambda: settings.OBJECTIVE,
 )
 @click.option("-t", "--task", help="Task", prompt=True, default=lambda: settings.TASK)
+@click.option("-m", "--model", help="Model", default=lambda: settings.OPENAI_MODEL_NAME)
 @click.option("-v", "--verbose", is_flag=True, help="Run in verbose mode.")
-def main(objective: str, task: str, verbose: bool):
+def main(objective: str, task: str, model, verbose: bool):
     print(f"Executing a plan for this objective: {objective}")
-    writemore(objective, task, verbose)
+    writemore(objective, task, model_name=model, verbose=verbose)
 
 
 if __name__ == "__main__":
